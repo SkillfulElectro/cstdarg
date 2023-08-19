d44ddd2691025f4dbba74ae5fd9997f1cc0a28c0
SkillfulElectro <97535877+SkillfulElectro@users.noreply.github.com>
2023-08-19 21:25:53 +0330

    Update cstdargHomeMade.h

--- cstdargHomeMade.h
+++ cstdargHomeMade.h
@@ -1,95 +1,74 @@
 #pragma once
 
-
 /*
-	so we gonna explore how cstdarg works here
+    Exploring the Functionality of cstdarg through Custom Class: Ellipsis
 */
 class Ellipsis {
-	/*
-		so basically first we make a object which with class type va_list
-		our Ellipsis class here !
-	*/
+    /*
+        To simulate the behavior of va_list, we create an object of the Ellipsis class.
+    */
 private:
-	void* Ellipsis_ptr;
-	// here we keep address of first ellipsis parameter of our function
+    void* Ellipsis_ptr;
+    // Store the address of the first ellipsis parameter of our function.
+
 public:
-	template <typename noneEllipsisType>
-	void setEllipsis(noneEllipsisType& last_nonEllipsis) {
-		/*
-			if u remember with va_start() , it would take our last none
-			Ellipsis element and based on that it would give us ellipsis
-			elements !
+    template <typename noneEllipsisType>
+    void setEllipsis(noneEllipsisType& last_nonEllipsis) {
+        /*
+            Mimicking va_start(): determine ellipsis elements based on the last non-Ellipsis element's type.
+        */
+        void* address = &last_nonEllipsis;
+        Ellipsis_ptr = static_cast<double*>(address) + 1;
+        /*
+            The core of our class: calculating the address of the first ellipsis element.
 
-			// so what we do here is similar to that the only diff thing we do is
-			that we take what is the biggest type in an parameters too !
-			*			so what i mean by biggest type ?
-			*				so it means double ( 8 byte ) in visual studio 2022
-		*/
+            The ellipsis parameters are stored with gaps equal to the size of the largest type.
+        */
+    }
 
-		void* address = &last_nonEllipsis;
-		Ellipsis_ptr = static_cast<double*>(address) + 1;
-		/*
-			so the heart of our class is here ! how do we get the address of it?
+    // Fetch the variable with the type that corresponds to the ellipsis.
+    // 
+    // In this method, the user provides a sample type to help the system deduce the type.
+    template<typename EllipsisType>
+    EllipsisType next_arg(EllipsisType sample) {
+        /*
+            Iterating through our array of parameters.
+            We ask the user for the ellipsis type and extract it from the array.
+        */
+        EllipsisType value = *(static_cast<EllipsisType*>(Ellipsis_ptr));
+        /*
+            Reading the value of our ellipsis element from the array.
+        */
+        Ellipsis_ptr = static_cast<double*>(Ellipsis_ptr) + 1;
+        /*
+            Moving to the next ellipsis element for the next call.
+            To restart, recall the setEllipsis() function.
+        */
 
-			// if u remember that we mentioned earlier functions keeps their
-			parameters in like an array and between each parameter there is
-			gap as much as the the biggest type ( a lil more than that )
-			element #Sponge_Bob_theory ! so if we want
-			// so if want to reach to our 1st ellipsis element we will jump from
-			that gap with making our address the biggest type pointer and plus it
-			with one ! here it is , our 1st ellipsis element !!!
-		*/
-	}
+        return value;
+    }
 
-	//give variable with the type which is in the ellpsis!
-	// 
-	// in this method user gives smth similar to that type to system deduces the type
-	template<typename EllipsisType>
-	EllipsisType next_arg(EllipsisType smth) {
-		/*
-			here we gonna walk through our array of parameters !
-			what we do is 1st we ask our user for the type of the ellipsis
-			and then we extract it from the array
-		*/
-		EllipsisType valuee = *(static_cast<EllipsisType*>(Ellipsis_ptr));
-		/*
-			reading value of our ellipsis element from array
-		*/
-		Ellipsis_ptr = static_cast<double*>(Ellipsis_ptr) + 1;
-		/*
-			and now jumping to next ellipsis element for next call !
+    // This version allows you to provide the value type to be read within <>
+    template<typename EllipsisType>
+    EllipsisType next_arg() {
+        /*
+            Similar to the previous method, we traverse the array of parameters.
+            User provides the ellipsis type and we extract it from the array.
+        */
+        EllipsisType value = *(static_cast<EllipsisType*>(Ellipsis_ptr));
+        /*
+            Reading the value of our ellipsis element from the array.
+        */
+        Ellipsis_ptr = static_cast<double*>(Ellipsis_ptr) + 1;
+        /*
+            Moving to the next ellipsis element for the next call.
+            To restart, recall the setEllipsis() function.
+        */
 
-			// for restarting the jumper , u have to recall setEllipsis() function
-		*/
-
-		return valuee;
-	}
-
-	// this one u provide the value to read inside of <> !
-	template<typename EllipsisType>
-	EllipsisType next_arg() {
-		/*
-			here we gonna walk through our array of parameters !
-			what we do is 1st we ask our user for the type of the ellipsis
-			and then we extract it from the array
-		*/
-		EllipsisType valuee = *(static_cast<EllipsisType*>(Ellipsis_ptr));
-		/*
-			reading value of our ellipsis element from array
-		*/
-		Ellipsis_ptr = static_cast<double*>(Ellipsis_ptr) + 1;
-		/*
-			and now jumping to next ellipsis element for next call !
-
-			// for restarting the jumper , u have to recall setEllipsis() function
-		*/
-
-		return valuee;
-	}
+        return value;
+    }
 };
 
 /*
-
-		//CopyRight SkillFulElectro LLC***
-
-*/	
+    Copyright © SkillFulElectro LLC.
+*/
